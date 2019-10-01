9ede45306e990537b6f943ba5d3506a15ed3ace8
LCzoboriek <56021888+LCzoboriek@users.noreply.github.com>
2019-10-01 09:59:34 +0100

    Add files via upload

---
+++ Payroll example.py
@@ -0,0 +1,9 @@
+# In this example im creating a simple payroll program
+
+name = input('What is your name? ')
+age = input('What is your age? ')
+Hours_worked = float(input('How many hours did you work? '))
+Hourly_rate = float(input('What is your hourly rate? '))
+print(name)
+print(age)
+print('Your total pay for the week: ', Hourly_rate*Hours_worked)
